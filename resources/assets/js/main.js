f53c9ee9a0c7d8e9977b29ad4e2968bb02b2e660
Andrea <andrea.buisset@hotmail.fr>
2016-05-09 17:08:14 +0200

    First commit : Mise en place du projet

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,8 +1,28 @@
+$(function(){
+	var app = {
+		init : function(){
+			var mdEditor = CodeMirror(document.getElementById('container'), {
+				value: "# Hello",
+				mode:  "text/x-markdown",
+				lineNumbers: true,
+				theme: "default"
+			})
 
-var React = require('react');
-var ReactDOM = require('react-dom');
+			var htmlEditor = CodeMirror(document.getElementById('container'), {
+				value: "<h1>Hello</h1>",
+				mode: "text/html",
+				lineNumbers: true,
+				theme: "default"
+			})
+			app.getText(mdEditor);
+		},
+		getText : function(editor){
+			$('.CodeMirror').on('keypress', function(){
+				var text = editor.getValue();
+				console.log(text);
+			});
+		},
+	}
+	app.init();
+});
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('hello')
-);
